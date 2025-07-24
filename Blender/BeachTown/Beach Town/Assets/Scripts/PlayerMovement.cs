6302923479ed65f6edbce0c865f619da4fdc2d63
Lindsey Welton <10878168@uvu.edu>
2025-07-24 00:06:32 -0600

    Beach Town
    - speed capped at 7 in script

--- Blender/BeachTown/Beach Town/Assets/Scripts/PlayerMovement.cs
+++ Blender/BeachTown/Beach Town/Assets/Scripts/PlayerMovement.cs
@@ -60,4 +60,16 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
     }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
 }
